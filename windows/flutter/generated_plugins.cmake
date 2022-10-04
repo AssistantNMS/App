05b1c044678d49b32dc736c555d3eaf7cfb66616
Kurt Lourens <hi@kurtlourens.com>
2022-10-04 15:52:13 +0200

    ⬆️ Update packages

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   bitsdojo_window_windows
   connectivity_plus_windows
+  dynamic_color
   flutter_secure_storage_windows
   platform_device_id_windows
   url_launcher_windows
