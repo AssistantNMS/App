18e30c23b3d7508edad20aa606efcc058a2ee0d5
Kurt Lourens <hi@kurtlourens.com>
2022-10-31 14:25:02 +0100

    🔖 2.1.1

--- lib/env/appVersionNum.dart
+++ lib/env/appVersionNum.dart
@@ -1,3 +1,3 @@
-const appsBuildNum = 294;
-const appsBuildName = '2.1.0';
-const appsCommit = '0edc4bf23d4d32d1459a5c79964a5bd3f718ba51';
+const appsBuildNum = 295;
+const appsBuildName = '2.1.1';
+const appsCommit = '9e0f1c3470e1a1314c83afe5d20bfdb8313221d7';
