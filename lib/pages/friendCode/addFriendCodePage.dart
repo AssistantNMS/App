1f369575aa331d5b231cc6021be265fdbbf2c2a9
Kurt Lourens <hi@kurtlourens.com>
2022-11-08 13:40:15 +0100

    🔥 Disable new Switch friendcode validator | Related to #135

--- lib/pages/friendCode/addFriendCodePage.dart
+++ lib/pages/friendCode/addFriendCodePage.dart
@@ -62,13 +62,13 @@ class _AddFriendCodeState extends State<AddFriendCodePage> {
   setCodeValidator(bool isSwitch) {
     String friendCodeMask = '@@@@-@@@@-@@@@@';
     String text = _codeController?.text ?? '';
-    if (isSwitch) {
-      friendCodeMask = '@@-@@@@-@@@@-@@@@';
-      _validationMap['code'] = () => switchFriendCodeValidator(text);
-    } else {
-      friendCodeMask = '@@@@-@@@@-@@@@@';
-      _validationMap['code'] = () => friendCodeValidator(text);
-    }
+    // if (isSwitch) {
+    //   friendCodeMask = '@@-@@@@-@@@@-@@@@';
+    //   _validationMap['code'] = () => switchFriendCodeValidator(text);
+    // } else {
+    friendCodeMask = '@@@@-@@@@-@@@@@';
+    _validationMap['code'] = () => friendCodeValidator(text);
+    // }
 
     if (_codeController == null ||
         (_codeController as dynamic).mask != friendCodeMask) {
@@ -100,73 +100,14 @@ class _AddFriendCodeState extends State<AddFriendCodePage> {
   Widget build(BuildContext context) {
     return StoreConnector<AppState, FriendCodeSettingsViewModel>(
       converter: (store) => FriendCodeSettingsViewModel.fromStore(store),
-      builder: (_, friendCodeSettingVm) {
+      builder: (_, FriendCodeSettingsViewModel friendCodeSettingVm) {
         bool isSwitch = friendCodeSettingVm.lastPlatformIndex == 3;
         setCodeValidator(isSwitch);
         return basicGenericPageScaffold(
           context,
           title: getTranslations().fromKey(LocaleKey.friendCode),
-          body: getBody(context, friendCodeSettingVm),
-          fab: FloatingActionButton(
-            onPressed: () async {
-              if (!_allValidationsPassed()) {
-                setState(() {
-                  _showValidation = true;
-                });
-                return;
-              }
-
-              LocalizationMap currentLocal =
-                  getTranslations().getCurrentLocalizationMap(
-                context,
-                friendCodeSettingVm.selectedLanguage,
-              );
-              AddFriendCodeViewModel apiObj = AddFriendCodeViewModel(
-                name: friendCodeVm.name,
-                code: friendCodeVm.code,
-                email: friendCodeVm.email,
-                platformType: friendCodeSettingVm.lastPlatformIndex + 1,
-                languageCode: currentLocal.code,
-              );
-              setState(() {
-                _isLoading = true;
-              });
-              Result submissionResult =
-                  await getApiRepo().submitFriendCode(apiObj);
-              setState(() {
-                _isLoading = false;
-              });
-              if (submissionResult.isSuccess) {
-                String thankYou = getTranslations()
-                    .fromKey(LocaleKey.thankYouForSubmittingFriendCode);
-                String checkMail = getTranslations()
-                    .fromKey(LocaleKey.pleaseCheckMailForConfirmation)
-                    .replaceAll('{0}', '(${apiObj.email})');
-                String dialogDescription = '$thankYou $checkMail';
-                prettyDialog(
-                  context,
-                  '${getPath().imageAssetPathPrefix}/email.png',
-                  getTranslations().fromKey(LocaleKey.friendCode),
-                  dialogDescription,
-                  onlyCancelButton: true,
-                );
-              } else {
-                getDialog().showSimpleDialog(
-                  context,
-                  getTranslations().fromKey(LocaleKey.error),
-                  Text(getTranslations()
-                      .fromKey(LocaleKey.friendCodeNotSubmitted)),
-                  buttonBuilder: (BuildContext ctx) => [
-                    getDialog().simpleDialogCloseButton(ctx),
-                  ],
-                );
-              }
-            },
-            heroTag: 'addFriendCode',
-            child: const Icon(Icons.check),
-            foregroundColor: getTheme().fabForegroundColourSelector(context),
-            backgroundColor: getTheme().fabColourSelector(context),
-          ),
+          body: getBody(context, friendCodeSettingVm, isSwitch),
+          fab: isSwitch ? null : getFab(friendCodeSettingVm),
         );
       },
     );
@@ -175,10 +116,10 @@ class _AddFriendCodeState extends State<AddFriendCodePage> {
   Widget getBody(
     BuildContext context,
     FriendCodeSettingsViewModel friendCodeVm,
+    bool isSwitch,
   ) {
     if (_isLoading) return getLoading().fullPageLoading(context);
     List<Widget> widgets = List.empty(growable: true);
-    bool isSwitch = friendCodeVm.lastPlatformIndex == 3;
 
     EdgeInsets textEditingPadding =
         const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0);
@@ -193,77 +134,145 @@ class _AddFriendCodeState extends State<AddFriendCodePage> {
       ),
     ));
 
-    widgets.add(Padding(
-      padding: textEditingPadding,
-      child: TextField(
-        controller: _nameController,
-        decoration: getTextFieldDecoration(
-          context,
-          LocaleKey.name,
-          _validationMap['name'],
-          _showValidation,
-        ),
-        onChanged: (_) => updateApiObj(),
-      ),
-    ));
-
-    widgets.add(Stack(
-      children: [
-        Padding(
-          padding: textEditingPadding,
-          child: TextField(
-            controller: _emailController,
-            decoration: getTextFieldDecoration(
-              context,
-              LocaleKey.emailAddress,
-              _validationMap['email'],
-              _showValidation,
-            ),
-            onChanged: (_) => updateApiObj(),
+    if (isSwitch) {
+      widgets.add(Padding(
+        padding: textEditingPadding,
+        child: const Center(child: Text('Not available yet')),
+      ));
+    } else {
+      widgets.add(Padding(
+        padding: textEditingPadding,
+        child: TextField(
+          controller: _nameController,
+          decoration: getTextFieldDecoration(
+            context,
+            LocaleKey.name,
+            _validationMap['name'],
+            _showValidation,
           ),
+          onChanged: (_) => updateApiObj(),
         ),
-        Positioned(
-          top: 4,
-          right: 22,
-          bottom: 4,
-          child: GestureDetector(
-            child: const Icon(Icons.help_outline),
-            onTap: () {
-              getDialog().showSimpleHelpDialog(
+      ));
+
+      widgets.add(Stack(
+        children: [
+          Padding(
+            padding: textEditingPadding,
+            child: TextField(
+              controller: _emailController,
+              decoration: getTextFieldDecoration(
                 context,
-                getTranslations().fromKey(LocaleKey.help),
-                getTranslations().fromKey(LocaleKey.weDoNotStoreEmails),
-              );
-            },
+                LocaleKey.emailAddress,
+                _validationMap['email'],
+                _showValidation,
+              ),
+              onChanged: (_) => updateApiObj(),
+            ),
           ),
-        )
-      ],
-    ));
-
-    widgets.add(Padding(
-      padding: textEditingPadding,
-      child: TextField(
-        controller: _codeController,
-        decoration: getTextFieldDecoration(
-          context,
-          LocaleKey.friendCode,
-          _validationMap['code'],
-          _showValidation,
-        ),
-        onChanged: (_) => updateApiObj(),
-        textCapitalization: TextCapitalization.characters,
-        inputFormatters: [
-          FilteringTextInputFormatter(RegExp('[A-z0-9]'), allow: true),
-          LengthLimitingTextInputFormatter(isSwitch ? 17 : 15),
+          Positioned(
+            top: 4,
+            right: 22,
+            bottom: 4,
+            child: GestureDetector(
+              child: const Icon(Icons.help_outline),
+              onTap: () {
+                getDialog().showSimpleHelpDialog(
+                  context,
+                  getTranslations().fromKey(LocaleKey.help),
+                  getTranslations().fromKey(LocaleKey.weDoNotStoreEmails),
+                );
+              },
+            ),
+          )
         ],
-      ),
-    ));
+      ));
+
+      widgets.add(Padding(
+        padding: textEditingPadding,
+        child: TextField(
+          controller: _codeController,
+          decoration: getTextFieldDecoration(
+            context,
+            LocaleKey.friendCode,
+            _validationMap['code'],
+            _showValidation,
+          ),
+          onChanged: (_) => updateApiObj(),
+          textCapitalization: TextCapitalization.characters,
+          inputFormatters: [
+            FilteringTextInputFormatter(RegExp('[A-z0-9]'), allow: true),
+            LengthLimitingTextInputFormatter(isSwitch ? 17 : 15),
+          ],
+        ),
+      ));
+    }
 
     widgets.add(emptySpace8x());
 
     return listWithScrollbar(
       itemCount: widgets.length,
       itemBuilder: (context, index) => widgets[index],
+    );
+  }
+
+  FloatingActionButton getFab(FriendCodeSettingsViewModel friendCodeSettingVm) {
+    return FloatingActionButton(
+      onPressed: () async {
+        if (!_allValidationsPassed()) {
+          setState(() {
+            _showValidation = true;
+          });
+          return;
+        }
+
+        LocalizationMap currentLocal =
+            getTranslations().getCurrentLocalizationMap(
+          context,
+          friendCodeSettingVm.selectedLanguage,
+        );
+        AddFriendCodeViewModel apiObj = AddFriendCodeViewModel(
+          name: friendCodeVm.name,
+          code: friendCodeVm.code,
+          email: friendCodeVm.email,
+          platformType: friendCodeSettingVm.lastPlatformIndex + 1,
+          languageCode: currentLocal.code,
+        );
+        setState(() {
+          _isLoading = true;
+        });
+        Result submissionResult = await getApiRepo().submitFriendCode(apiObj);
+        setState(() {
+          _isLoading = false;
+        });
+        if (submissionResult.isSuccess) {
+          String thankYou = getTranslations()
+              .fromKey(LocaleKey.thankYouForSubmittingFriendCode);
+          String checkMail = getTranslations()
+              .fromKey(LocaleKey.pleaseCheckMailForConfirmation)
+              .replaceAll('{0}', '(${apiObj.email})');
+          String dialogDescription = '$thankYou $checkMail';
+          prettyDialog(
+            context,
+            '${getPath().imageAssetPathPrefix}/email.png',
+            getTranslations().fromKey(LocaleKey.friendCode),
+            dialogDescription,
+            onlyCancelButton: true,
+          );
+        } else {
+          getDialog().showSimpleDialog(
+            context,
+            getTranslations().fromKey(LocaleKey.error),
+            Text(getTranslations().fromKey(LocaleKey.friendCodeNotSubmitted)),
+            buttonBuilder: (BuildContext ctx) => [
+              getDialog().simpleDialogCloseButton(ctx),
+            ],
+          );
+        }
+      },
+      heroTag: 'addFriendCode',
+      child: const Icon(Icons.check),
+      foregroundColor: getTheme().fabForegroundColourSelector(context),
+      backgroundColor: getTheme().fabColourSelector(context),
     );
   }
 
