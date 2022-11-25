488f99a23a440ad8b6c2e9f332f69b94b64259d1
Kurt Lourens <hi@kurtlourens.com>
2022-11-25 12:15:46 +0100

    🔖 2.3.0

--- lib/assistantAppsSettings.dart
+++ lib/assistantAppsSettings.dart
@@ -1,3 +1,3 @@
 const assistantAppsApiUrl = 'https://api.assistantapps.com';
 const assistantAppsAppGuid = '589405b4-e40f-4cd9-b793-6bf37944ee09';
-const currentWhatIsNewGuid = 'e9a23e18-2eac-44ab-98f0-53f3fd39a7ae';
+const currentWhatIsNewGuid = '5d286c7c-2c29-40c7-a0fa-998c25ec845a';
