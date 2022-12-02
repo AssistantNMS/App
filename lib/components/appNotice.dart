8dffc10dd83ca195202217052eda635cfe64e93a
Kurt Lourens <hi@kurtlourens.com>
2022-12-02 19:14:36 +0100

    ✨ App Notice feature

---
+++ lib/components/appNotice.dart
@@ -0,0 +1,34 @@
+import 'package:assistantapps_flutter_common/assistantapps_flutter_common.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/NmsUIConstants.dart';
+
+Widget appNoticeTile(AppNoticeViewModel notice) {
+  void Function() onTap = () => {};
+
+  if (notice.externalUrl.isNotEmpty) {
+    onTap = () => launchExternalURL(notice.externalUrl);
+  }
+
+  return flatCard(
+    child: Padding(
+      padding: const EdgeInsets.all(4),
+      child: ListTile(
+        leading: ClipRRect(
+          borderRadius: NMSUIConstants.gameItemBorderRadius,
+          child: networkImage(notice.iconUrl, width: 50.0),
+        ),
+        title: Text(notice.name, maxLines: 1),
+        subtitle: Text(notice.subtitle, maxLines: 1),
+        trailing: (notice.externalUrl.isEmpty)
+            ? null
+            : IconButton(
+                icon: const Icon(Icons.chevron_right),
+                iconSize: 32,
+                onPressed: onTap,
+              ),
+        onTap: onTap,
+      ),
+    ),
+  );
+}
