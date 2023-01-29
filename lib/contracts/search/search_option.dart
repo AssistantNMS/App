8f8df7cd1a72547e57e27c42863a452e6d887b75
Lenni009 <guentherhanslphdyt@gmail.com>
2023-01-29 17:30:31 +0100

    contracts/search

--- lib/contracts/search/search_option.dart
+++ lib/contracts/search/search_option.dart
@@ -1,6 +1,6 @@
 import 'package:assistantapps_flutter_common/assistantapps_flutter_common.dart';
 
-import 'searchOptionType.dart';
+import 'search_option_type.dart';
 
 class SearchOption {
   LocaleKey title;
