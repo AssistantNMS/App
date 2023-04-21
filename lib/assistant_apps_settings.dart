3bcc01f9714042c67c5180c92bb0e30383529364
Kurt Lourens <kurt.lourens@outlook.com>
2023-04-21 20:38:05 +0200

    📝 Update version guid

--- lib/assistant_apps_settings.dart
+++ lib/assistant_apps_settings.dart
@@ -1,3 +1,3 @@
 const assistantAppsApiUrl = 'https://api.assistantapps.com';
 const assistantAppsAppGuid = '589405b4-e40f-4cd9-b793-6bf37944ee09';
-const currentWhatIsNewGuid = '273ac19d-a63e-4457-b4fd-0803407c6471';
+const currentWhatIsNewGuid = 'f3253179-d7bb-4c4b-a123-e7578d419b04';
