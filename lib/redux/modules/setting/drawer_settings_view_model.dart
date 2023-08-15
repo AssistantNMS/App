e9d80536f089b6fcef04e81d073857221e21eed7
Kurt Lourens <hi@kurtlourens.com>
2023-08-15 18:07:58 +0200

    ✨ Conditionally show drawer items based on user set homepage

--- lib/redux/modules/setting/drawer_settings_view_model.dart
+++ lib/redux/modules/setting/drawer_settings_view_model.dart
@@ -1,20 +1,24 @@
 import 'package:redux/redux.dart';
 
+import '../../../contracts/enum/homepage_type.dart';
 import '../../../contracts/redux/app_state.dart';
 import 'selector.dart';
 
 class DrawerSettingsViewModel {
   final bool dontShowSpoilerAlert;
   final bool isPatron;
+  final HomepageType homepageType;
 
   DrawerSettingsViewModel({
     required this.dontShowSpoilerAlert,
     required this.isPatron,
+    required this.homepageType,
   });
 
   static DrawerSettingsViewModel fromStore(Store<AppState> store) =>
       DrawerSettingsViewModel(
         dontShowSpoilerAlert: getDontShowSpoilerAlert(store.state),
         isPatron: getIsPatron(store.state),
+        homepageType: getHomepageType(store.state),
       );
 }
