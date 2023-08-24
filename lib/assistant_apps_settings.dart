bf1a7f233363b005df50b8f60e78c6c8036e754c
Kurt Lourens <hi@kurtlourens.com>
2023-08-24 19:43:34 +0200

    🔖 2.17.0

--- lib/assistant_apps_settings.dart
+++ lib/assistant_apps_settings.dart
@@ -1,3 +1,3 @@
 const assistantAppsApiUrl = 'https://api.assistantapps.com';
 const assistantAppsAppGuid = '589405b4-e40f-4cd9-b793-6bf37944ee09';
-const currentWhatIsNewGuid = '07c28633-80c6-4e6d-8257-048b62ca1b82';
+const currentWhatIsNewGuid = 'bd4f8f68-f691-45e0-b8c7-dfed36c2892a';
--- lib/env/app_version_num.dart
+++ lib/env/app_version_num.dart
@@ -1,3 +1,3 @@
-const appsBuildNum = 318;
-const appsBuildName = '2.16.2';
-const appsCommit = '988169e95917516e24c5216544ef5ad0ffcbf901';
+const appsBuildNum = 319;
+const appsBuildName = '2.17.0';
+const appsCommit = '7fd3ec0621cfb9b87c32e669d64927642e6e1d06';
