06a28d516e2ade945c6d867b0cef2d404f746c55
Kurt Lourens <kurt.lourens@outlook.com>
2024-01-01 14:57:12 +0100

    🐛 Add logic to handle iOS notification permission

--- lib/services/base/notification_service.dart
+++ lib/services/base/notification_service.dart
@@ -5,20 +5,41 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import '../../integration/dependency_injection.dart';
 
 class NotificationService implements INotificationService {
+  bool setupComplete = false;
+
   NotificationService() {
     if (isWindows) return;
-    Firebase.initializeApp().then((_) {
-      FirebaseMessaging.instance
+    Firebase.initializeApp().then(handleToken);
+  }
+
+  handleToken(FirebaseApp fApp) async {
+    if (isiOS) {
+      String? apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+      if (apnsToken == null) {
+        await Future<void>.delayed(
+          const Duration(
+            seconds: 3,
+          ),
+        );
+        apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+      }
+    }
+    try {
+      await FirebaseMessaging.instance
           .requestPermission(sound: true, badge: true, alert: true);
-      FirebaseMessaging.instance.getToken().then((token) {
-        getLog().d("Firebase Token: $token");
-      });
-    });
+      String? token = await FirebaseMessaging.instance.getToken();
+      getLog().d("Firebase Token: $token");
+      setupComplete = true;
+    } catch (ex) {
+      //
+    }
   }
 
   @override
   subscribeToTopics(context, String selectedLanguage) {
     bool isProduction = getEnv().isProduction;
+    if (setupComplete == false) return;
+
     for (String code in getLanguage().supportedLanguagesCodes()) {
       if (code == selectedLanguage) {
         getLog().i("Sub Topic: $selectedLanguage");
