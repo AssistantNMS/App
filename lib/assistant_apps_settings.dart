eaa6bccfda8823fc2a421aaf392d961f10e0fba0
Kurt Lourens <hi@kurtlourens.com>
2024-02-05 10:52:18 +0100

    📝 Update what is new guid

--- lib/assistant_apps_settings.dart
+++ lib/assistant_apps_settings.dart
@@ -1,3 +1,3 @@
 const assistantAppsApiUrl = 'https://api.assistantapps.com';
 const assistantAppsAppGuid = '589405b4-e40f-4cd9-b793-6bf37944ee09';
-const currentWhatIsNewGuid = '4a32332b-3831-4a68-93a2-f65ae33ec048';
+const currentWhatIsNewGuid = 'aff679cd-cc9a-4ed7-85e6-c1afc917e06e';
