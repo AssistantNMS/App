50f26b2b0374a6f460e4560733fb76538aeb64b0
Kurt Lourens <kurt.lourens@outlook.com>
2024-09-29 10:54:33 +0200

    🔖 2.28.1

--- lib/env/app_version_num.dart
+++ lib/env/app_version_num.dart
@@ -1,3 +1,3 @@
 const appsBuildNum = 344;
 const appsBuildName = '2.28.1';
-const appsCommit = '6babe1c5c21010cab9220faf8a941c161ba514a5';
+const appsCommit = '5e415b6e6a27491beb20e455d52dc8824d51c082';
